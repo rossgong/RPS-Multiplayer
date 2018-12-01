aec3e9b551bbb8d00ccb412fc80e8bc13ff9b39d
Ross Gongaware <rossgong@gmail.com>
2018-11-30 21:06:35 -0500

    added basic structure to app

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var config = {
+	apiKey: "AIzaSyDfSzcQGSx-DL5ODc0MjuMpn-4vOM9Orns",
+	authDomain: "gongaware-test.firebaseapp.com",
+	databaseURL: "https://gongaware-test.firebaseio.com",
+	projectId: "gongaware-test",
+	storageBucket: "gongaware-test.appspot.com",
+	messagingSenderId: "759532035576"
+};
+firebase.initializeApp(config);
+
